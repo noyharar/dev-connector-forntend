e028b05c4ada672f438fe915a9ed947692ad7c85
noyharar <noyharar@post.bgu.ac.il>
2023-02-19 21:23:19 +0200

    create components Login and Register and init the routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-import React, {Fragment} from "react";
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import Navbar from "./Components/layout/Navbar";
 import Landing from "./Components/layout/Landing";
-const App = () =>
-    <Fragment>
-        <Navbar/>
-        <Landing/>
-    </Fragment>
+import Register from "./Components/auth/Register";
+import Login from "./Components/auth/Login";
 
-
+const App = () => {
+    return (
+        <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="login" element={<Login />} />
+                </Routes>
+            </Router>
+    )
+};
 export default App;
