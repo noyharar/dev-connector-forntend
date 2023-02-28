80c3c8e15034edac739b6fd2729c88b2833812df
noyharar <noyharar@post.bgu.ac.il>
2023-02-28 14:44:01 +0200

    create profile action

--- src/Components/profile-forms/CreateProfile.js
+++ src/Components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, {useState,Fragment} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from "react-redux";
+import {createProfile} from "../../actions/profile";
+import {Link} from "react-router-dom"
 
-const CreateProfile = props => {
+const CreateProfile = ({createProfile , history}) => {
     const [ formData, setFormData] = useState({
         company :'',
         website :'',
@@ -37,7 +39,10 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
-
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history)
+    }
     return (
         <Fragment>
                 <h1 className="large text-primary">
@@ -48,7 +53,7 @@ const CreateProfile = props => {
                     profile stand out
                 </p>
                 <small>* = required field</small>
-                <form className="form">
+                <form className="form" onSubmit={e => onSubmit(e)}>
                     <div className="form-group">
                         <select name="status" value={status} onChange={e => onChange(e)}>
                             <option value="0">* Select Professional Status</option>
@@ -146,11 +151,11 @@ const CreateProfile = props => {
     );
 }
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+    createProfile: PropTypes.func.isRequired,
+};
 
 
-const mapStateToProps = state => ({
-});
 
 export default connect(
-    mapStateToProps,)(CreateProfile);
+    null,{createProfile})(CreateProfile);
--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,8 +1,10 @@
 import {
     GET_PROFILE,
     PROFILE_ERROR,
+    CREATE_PROFILE
 } from "../actions/types";
 import axios from 'axios';
+import {setAlert} from "./alert";
 
 
 // Get current Profile
@@ -15,6 +17,38 @@ export const getCurrentProfile =  () => async (dispatch) => {
             payload: res.data
         });
     } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+//Create or update profile
+
+export const createProfile =  (formData, history, edit=false) => async (dispatch) => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        const res = await axios.post('http://localhost:5000/api/profile',  JSON.stringify(formData),config);
+
+        dispatch({
+            type: CREATE_PROFILE,
+            payload: res.data
+        });
+        dispatch(setAlert(edit ? 'Profile updated' : 'Profile created'));
+        if(!edit){
+            history.push('/dashboard')
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+        }
         dispatch({
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status}
